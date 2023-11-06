77678f2d3d2ce70142f77c1e7bedd004fb6f3723
Simon <SYe@scu.edu>
2023-11-05 20:20:32 -0800

    updating homne data

--- market/src/views/home.vue
+++ market/src/views/home.vue
@@ -2,8 +2,11 @@
   <div class="home">
     <Navbar />
     <header class="home-header">
-      <h1>Welcome to Our Marketplace</h1>
+      <h1>Welcome to Our Community Marketplace</h1>
       <p>Discover amazing products and deals!</p>
+      <!-- 添加一个输入框和显示消息的段落 -->
+      <input v-model="userMessage" placeholder="What are you looking for?">
+      <p>You are searching for: {{ userMessage }}</p>
     </header>
     <!-- 其他首页内容 -->
     <Footer />
@@ -19,6 +22,12 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  data() {
+    return {
+      // 添加一个新的数据属性
+      userMessage: ''
+    }
   }
 }
 </script>
@@ -28,5 +37,14 @@ export default {
   text-align: center;
   padding: 50px;
   background: #f5f5f5;
+}
+
+/* 为输入框添加一些基本样式 */
+input {
+  margin-top: 20px;
+  padding: 10px;
+  font-size: 16px;
+  width: 100%;
+  box-sizing: border-box;
 }
 </style>
