0b7d6857b5ab8afe8d3199d2f47827e7cbeb7086
suny0030 <73318489+suny0030@users.noreply.github.com>
2023-11-11 17:53:08 -0800

    Create login.vue

---
+++ login.vue
@@ -0,0 +1,63 @@
+<!-- Login.vue -->
+<template>
+  <div class="login-container">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="username" required>
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" required>
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      // Add your authentication logic here
+      // For example, you can make an API call to validate the username and password
+      console.log('Logging in with:', this.username, this.password);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+label {
+  display: block;
+  margin-bottom: 8px;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 16px;
+}
+
+button {
+  background-color: #007BFF;
+  color: #fff;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
