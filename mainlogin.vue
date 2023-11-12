310f44e288dbacd23a859185f6ee5303f0421b3e
suny0030 <73318489+suny0030@users.noreply.github.com>
2023-11-11 18:00:47 -0800

    Create mainlogin.vue

---
+++ mainlogin.vue
@@ -0,0 +1,10 @@
+<!-- main.js -->
+import Vue from 'vue';
+import App from './App.vue';
+import Login from './components/Login.vue';
+
+Vue.component('login', Login);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
