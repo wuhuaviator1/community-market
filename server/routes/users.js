97796ab0c3d4a57bf921076f9a33c009edbce2dd
Simon <SYe@scu.edu>
2023-11-18 18:28:42 -0800

    applying mongodb, and setup a sample data output in /users of the server folder

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+//
+// module.exports = router;
+
+
+// routes/users.js
+const connectMongoDB = require('../db');
+
+router.get('/', async function(req, res, next) {
+  try {
+    const db = await connectMongoDB();
+    const accounts = await db.collection("accounts").find({}).toArray();
+    res.json(accounts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("无法获取数据");
+  }
 });
 
 module.exports = router;
