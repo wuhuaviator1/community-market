e60bb963b3402fa2f42ddd600405cd3475e9bd1a
Joey <njjiangzhouyi@gmail.com>
2023-11-29 01:01:44 -0800

    m

--- server/app.js
+++ server/app.js
@@ -1,25 +1,30 @@
 // app.js
-require('dotenv').config();
-const express = require('express');
-const createError = require('http-errors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const connectMongoDB = require('./db'); // 导入db.js中的函数
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const ApiRouter = require('./routes/ApiRoutes');
+require("dotenv").config();
+const express = require("express");
+const createError = require("http-errors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const connectMongoDB = require("./db"); // 导入db.js中的函数
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const ApiRouter = require("./routes/ApiRoutes");
 const app = express();
 
-app.use(logger('dev'));
+//cors
+const cors = require("cors");
+app.use(cors());
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use("/api", ApiRouter);
 
 // ...省略错误处理器和404处理器的代码...
