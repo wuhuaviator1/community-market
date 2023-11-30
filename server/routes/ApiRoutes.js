9264bec5bd19859bb5bc5ce8670362a2f315bfa9
Simon <SYe@scu.edu>
2023-11-30 02:09:02 -0800

    server debug

--- server/routes/ApiRoutes.js
+++ server/routes/ApiRoutes.js
@@ -5,7 +5,6 @@ const userController = require("../controller/UserController");
 const goodsController = require("../controller/goodsController");
 const listingController = require("../controller/listingController");
 const cartController = require("../controller/cartController");
-const userUidController = require("../controller/UserUidController");
 // 用户路由
 // /api/users
 router.post("/users", userController.createUser);
@@ -14,9 +13,6 @@ router.get("/users/:id", userController.getUser);
 router.put("/users/profile/:uid", userController.updateUserProfile);
 router.delete("/users/:id", userController.deleteUser);
 
-// 用户UID
-router.get("/user/:uid", userUidController.getUserByUid);
-router.delete("/users/:id", userUidController.deleteUid);
 router.post("/login/:uid", userController.login);
 // 商品路由
 router.post("/goods", goodsController.createGoods);
