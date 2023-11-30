bf6dfe4549cfaf9ad5e1ef7d5c0f92349e4424b1
Jason Guo <jason.ghzr@gmail.com>
2023-11-29 23:16:36 -0800

    Update

--- market/src/components/Footer.vue
+++ market/src/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
   <footer class="footer">
+    <section>
+      <router-link to="/contact-us">Join Our Community: </router-link>
+      <a href="https://discord.gg/mH22gVxj">
+        <img id="logo" src="../assets/discordLogo.png" alt="Join us on Discord" />
+      </a>
+    </section>
+
     <p>&copy; 2023 Marketplace. All rights reserved.</p>
   </footer>
 </template>
@@ -12,9 +19,30 @@ export default {
 
 <style scoped>
 .footer {
-  background-color: #333;
+  display: flex;
+  justify-content: space-around;
+  background-color: black;
   color: white;
-  text-align: center;
+  text-align: end;
   padding: 20px 0;
+}
+
+.footer p{
+  margin-right: 1rem;
+}
+
+.footer a{
+  color: white;
+  font-size: 1.2rem;
+  text-decoration: none;
+}
+
+#logo{
+  height: 2rem;
+  width: 2rem;
+}
+
+section a{
+  padding-left: 1rem;
 }
 </style>
