2227a390855319202844f07d6a8e06ca437c93aa
Simon <SYe@scu.edu>
2023-11-30 03:06:19 -0800

    get user profile

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -15,15 +15,6 @@ exports.login = async (req, res) => {
   }
 };
 
-exports.createUser = async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(201).send(newUser);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
 exports.findUserByUId = async (req, res) => {
   try {
     const user = await User.findOne({ uid: req.params.uid });
@@ -35,33 +26,23 @@ exports.findUserByUId = async (req, res) => {
     res.status(500).send(error);
   }
 };
-// 获取所有用户
-exports.getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.send(users);
-  } catch (error) {
-    console.error("Error getting users:", error);
-    res.status(500).send("Internal Server Error");
-  }
-};
-
-// 获取单个用户
-exports.getUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id)
-      .populate("cart.items")
-      .populate("listings");
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
-
 // 更新用户信息
+exports.getUserProfileByUid = async (req, res) => {
+  try {
+    const uid = req.params.uid;
+
+    const user = await User.findOne({ uid: uid }, 'userProfile'); // 只查询 userProfile 字段
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send(user.userProfile);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
 exports.updateUserProfile = async (req, res) => {
   try {
     const uid = req.params.uid;
