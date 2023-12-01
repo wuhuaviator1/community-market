44aa3c95c456c4a403bf0ec2e66cdda1b390cabb
Jason Guo <jason.ghzr@gmail.com>
2023-11-30 19:10:44 -0800

    m

--- functions/controller/UserController.js
+++ functions/controller/UserController.js
@@ -2,10 +2,9 @@ const User = require("../model/UserModel");
 
 exports.login = async (req, res) => {
   try {
-    // const firebaseUid = req.body.uid;
-    let user = await User.findOne({uid: req.params.uid});
+    let user = await User.findOne({ uid: req.params.uid });
     if (!user) {
-      user = new User({uid: req.params.uid});
+      user = new User({ uid: req.params.uid });
       await user.save();
       res.status(201).send(user); // 新用户创建
     } else {
@@ -16,62 +15,43 @@ exports.login = async (req, res) => {
   }
 };
 
-exports.createUser = async (req, res) => {
+exports.findUserByUId = async (req, res) => {
   try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(201).send(newUser);
+    const user = await User.findOne({ uid: req.params.uid });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+// 更新用户信息
+exports.getUserProfileByUid = async (req, res) => {
+  try {
+    const uid = req.params.uid;
+
+    const user = await User.findOne({ uid: uid }, "userProfile"); // 只查询 userProfile 字段
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send(user.userProfile);
   } catch (error) {
     res.status(400).send(error);
   }
 };
-exports.findUserByUId = async (req, res) => {
-  try {
-    const user = await User.findOne({uid: req.params.uid});
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
-// 获取所有用户
-exports.getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.send(users);
-  } catch (error) {
-    console.error("Error getting users:", error);
-    res.status(500).send("Internal Server Error");
-  }
-};
 
-// 获取单个用户
-exports.getUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id)
-        .populate("cart.items")
-        .populate("listings");
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
-
-// 更新用户信息
 exports.updateUserProfile = async (req, res) => {
   try {
     const uid = req.params.uid;
     const userProfileUpdates = req.body.userProfile;
 
     const user = await User.findOneAndUpdate(
-        {uid: uid},
-        {$set: {userProfile: userProfileUpdates}},
-        {new: true, runValidators: true},
+      { uid: uid },
+      { $set: { userProfile: userProfileUpdates } },
+      { new: true, runValidators: true }
     );
 
     if (!user) {
