087190ac52fbc5cc5e623bda26f9e31591ca403a
Joey <njjiangzhouyi@gmail.com>
2023-11-30 17:59:59 -0800

    mes

--- server/routes/ApiRoutes.js
+++ server/routes/ApiRoutes.js
@@ -7,14 +7,17 @@ const listingController = require("../controller/listingController");
 const cartController = require("../controller/cartController");
 // 用户路由
 // /api/users
-router.get("/users/:uid", userController.findUserByUId)
-router.get('/users/profile/:uid', userController.getUserProfileByUid);
+router.get("/users/:uid", userController.findUserByUId);
+router.get("/users/profile/:uid", userController.getUserProfileByUid);
 router.put("/users/profile/:uid", userController.updateUserProfile);
 router.delete("/users/:id", userController.deleteUser);
 router.post("/login/:uid", userController.login);
 // 商品路由
-router.get('/getALlGoods',goodsController.getAllGoods)
-router.get('/getGoodBby/category/:category', goodsController.getGoodsByCategory);
-router.get('/getGoodBy/name/:name', goodsController.getGoodsByName);
-router.get('/getAllCategories', goodsController.getAllCategories);
+router.get("/getALlGoods", goodsController.getAllGoods);
+router.get(
+  "/getGoodBby/category/:category",
+  goodsController.getGoodsByCategory
+);
+router.get("/getGoodBy/name/:name", goodsController.getGoodsByName);
+router.get("/getAllCategories", goodsController.getAllCategories);
 module.exports = router;
